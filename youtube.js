614770637cfadbaa0097e5d74543c5f1d3754af4
Naufal Rasyada <39319012+NaufalR111@users.noreply.github.com>
2021-02-16 20:43:05 +0700

    Update youtube.js

--- youtube.js
+++ youtube.js
@@ -1,7 +1,45 @@
-var playersrc=$('#ytplayer').attr('src');
-$('#ytplayer').mouseover(function(){
-$('#ytplayer').attr('src',playersrc+'&autoplay=1');
-});
-$('#ytplayer').mouseout(function(){
-$('#ytplayer').attr('src',playersrc);
+jQuery(document).ready(function($) {
+Â Â Â Â $('#youtube-video').mediaelementplayer();
+
+
+    $('#youtube-video').on('mouseenter', function(){
+
+    	var mute = $(this).hasClass('muted');
+
+        $(this).find("button").each( function( i, el) {
+            var elem = $( el );
+            if(elem.attr("title") == 'Play'){
+                elem.click();
+            }
+
+            console.log(mute);
+
+            if(mute){
+	            if(elem.attr("title") == 'Mute'){
+	                elem.click();
+	            }
+            }
+        })
+    }).on('mouseleave', function(){
+        $(this).find("button").each( function( i, el ) {
+            var elem = $( el );
+            if(elem.attr("title") == 'Pause'){
+                elem.click();
+            }
+        })
+    });
+
+    $('#html-video').each(function(i, obj) {
+        $(this).on("mouseover", function() {  
+        	if($(this).hasClass('muted')){
+        		this.muted = true;
+        	}
+        	this.play();
+        });
+        $(this).on("mouseout", function() { 
+        	this.muted = false;
+        	this.pause();
+        });
+    });
+
 });
